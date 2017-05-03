e6ba37687263b681283dda276f8c38b3c85df4f2
matthew-zak <matthew.zak@tooploox.com>
2017-05-03 16:27:39 -0400

    Initial commit. Create UI

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Admin on 02/05/2017.
+//  Copyright © 2017 Mattowy. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
