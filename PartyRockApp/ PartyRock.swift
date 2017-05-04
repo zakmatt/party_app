88bc670cebdfec832c1b1c0dd7ed42f91e99b8b5
matthew-zak <matthew.zak@tooploox.com>
2017-05-04 00:22:15 -0400

    create model

---
+++ PartyRockApp/ PartyRock.swift
@@ -0,0 +1,10 @@
+//
+//   PartyRock.swift
+//  PartyRockApp
+//
+//  Created by Admin on 04/05/2017.
+//  Copyright © 2017 Mattowy. All rights reserved.
+//
+
+import Foundation
+
---
+++ PartyRockApp/PartyCell.swift
@@ -0,0 +1,23 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Admin on 03/05/2017.
+//  Copyright © 2017 Mattowy. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func updateUI() {
+        
+    }
+}
